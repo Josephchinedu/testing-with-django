25d6c28dcf7449edcd283f8aed04bc21e793eea9
Joseph chinedu <josephchinedu34@gmail.com>
2022-06-02 18:01:25 +0100

    test setup

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+class ModelTesting(TestCase):
+    def setUp(self) -> None:
+
+        self.blog = Post.objects.create(title='Blog', author='Author', slug='blog')
+
+    def test_post_model(self):
+        self.assertEqual(self.blog.title, 'Blog')
+        self.assertEqual(self.blog.author, 'Author')
+        self.assertEqual(self.blog.slug, 'blog')
